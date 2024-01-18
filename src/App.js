7766d8e0e2d6b76d6eff135272ee6949c00f7954
Rafael-China <rcnfuncional@gmail.com>
2024-01-17 22:25:39 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="container-fluid divNav">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav >
+    </div >
+  );
+}
+
+export default App;
