6f0fa7afdaffc23237be9b30a000aacf058d57cf
Rafael-China <rcnfuncional@gmail.com>
2024-01-22 23:05:57 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,37 @@ import './App.css';
 
 function App() {
   return (
-    <div class="container-fluid divNav">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Disabled</a>
-            </li>
-          </ul>
+    <div childrena>
+      <div class="container-fluid divNav">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </nav >
+      </div >
+      <div class="container-fluid main">
+        <div class="wellcome">
+          <div class="cotainer divTexto">
+            <h2>Olá me chamo <h2 class="animate-charcter">Rafael </h2>, seja muito bem vindo ao <h2 class="animate-charcter">meu portifólio</h2></h2>
+            <p>Atualmente tenho 20 anos, sou estudante de Análise e Desenvolvimento de Sistemas na Universidade Newton Paiva.
+              <br /> Busco minha primeira vaga de trabalho na area de desenvolvimento Web.</p>
+          </div>
         </div>
-      </nav >
-    </div >
+      </div>
+    </div>
   );
 }
 
