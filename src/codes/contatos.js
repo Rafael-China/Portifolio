3d93f1a1efef83fd5c7655901ba9e11121f702be
Rafael-China <rcnfuncional@gmail.com>
2024-02-23 10:09:29 -0300

    new updates

--- src/codes/contatos.js
+++ src/codes/contatos.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/contatos.css';
+import { SiGithub, SiLinkedin, SiWhatsapp } from "react-icons/si";
 
 const contatos = () => {
     return (
@@ -15,10 +16,10 @@ const contatos = () => {
                         <a href='tel:+5531998165044'><p>(31) 99816-5044</p></a>
                     </div>
                     <div className='mediasClicavel'>
-                        <a>Linkedin</a>
-                        <a>Github</a>
-                        <a>Instagram</a>
-                        <a>wpp</a>
+                        <a href='https://www.linkedin.com/in/rafael-nogueira-49382b221/' target='blank_'><span><SiLinkedin /></span></a>
+                        <a href='#'><span><SiGithub /></span></a>
+                        {/* <a>Instagram</a> */}
+                        <a href='#'><span><SiWhatsapp /></span></a>
                     </div>
                 </div>
             </div>
